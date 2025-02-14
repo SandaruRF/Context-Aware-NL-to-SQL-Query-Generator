ad1b889f368af17b928f6127329a0d0554fd392d
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-15 00:40:43 +0530

    Minor changes

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:200266@localhost:5432/EagleCartDB")  #, "postgresql://user:password@localhost/dbname"
--- app/core/database.py
+++ app/core/database.py
@@ -2,16 +2,22 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, close_all_sessions
 from app.core.config import DATABASE_URL
 
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set!")
+
+print(f"Using database URL: {DATABASE_URL}")
+
+# Close all previous database sessions to ensure no lingering connections
 close_all_sessions()
 
 engine = create_engine(DATABASE_URL)
+print(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
     try:
-        db.rollback()
         yield db
     finally:
         db.close()
