f87cd572413aca985ab9f30615cf24a55d3804a9
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-16 20:13:10 +0530

    Minor changes

--- app/core/config.py
+++ app/core/config.py
@@ -5,11 +5,3 @@ load_dotenv(override=True)
 DATABASE_URL = os.getenv("") #DATABASE_URL
 if not DATABASE_URL:
     DATABASE_URL = "mysql+pymysql://root:200266@localhost/chinook"
-
-#mysql+pymysql://root:200266@localhost/chinook
-#postgresql://postgres:200266@localhost:5432/chinook
-#Microsoft SQL Server
-#MariaDB
-#Oracle Database
-#sqlite:///Chinook_Sqlite.sqlite
-#Amazon Redshift
