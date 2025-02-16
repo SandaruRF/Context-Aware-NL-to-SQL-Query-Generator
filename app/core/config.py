1d26c71df0092d539699b58448cd3c66fb80757e
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-16 20:10:10 +0530

    Implemet sql query generation and change code structure

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("") #DATABASE_URL
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:200266@localhost:5432/EagleCartDB"
-    
-print(f"DATABASE_URL: {DATABASE_URL}")
+    DATABASE_URL = "mysql+pymysql://root:200266@localhost/chinook"
+
+#mysql+pymysql://root:200266@localhost/chinook
+#postgresql://postgres:200266@localhost:5432/chinook
+#Microsoft SQL Server
+#MariaDB
+#Oracle Database
+#sqlite:///Chinook_Sqlite.sqlite
+#Amazon Redshift
