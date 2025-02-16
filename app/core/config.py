1d46ba2b4ea90627755c452c0db225599516d91d
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-16 21:37:29 +0530

    Minor changes

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-DATABASE_URL = os.getenv("") #DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     DATABASE_URL = "mysql+pymysql://root:200266@localhost/chinook"
