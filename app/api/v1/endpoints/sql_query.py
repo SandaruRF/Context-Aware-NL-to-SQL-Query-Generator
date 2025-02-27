d289434805b9cde0d81750baa92eb80c040c0450
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-27 23:55:01 +0530

    Retrieve data from database

--- app/api/v1/endpoints/sql_query.py
+++ app/api/v1/endpoints/sql_query.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 from app.core.database import get_db
 from app.services.sql_query import generate_sql_query
 
@@ -8,10 +9,17 @@ router = APIRouter()
 @router.get("/sql_query")
 async def get_sql_query(db: Session = Depends(get_db)):
     try:
-        nl_query = "Give list of albems with respective artist name"
+        nl_query = "Give list of albems that are created by 'acdc'"
         sql_query = await generate_sql_query(db, nl_query)
         if not sql_query:
             raise HTTPException(status_code=400, detail="Failed to generate SQL query.")
-        return sql_query
+        
+        result = db.execute(text(sql_query))
+        data = result.fetchall()
+
+        columns = result.keys()
+        records = [dict(zip(columns, row)) for row in data]
+
+        return {"query": sql_query, "data": records}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating SQL query: {str(e)}")
